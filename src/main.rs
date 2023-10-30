c51b16df0d1b1df73281e95c0918c9e57b850652
Duo Francis <duofrancis11@gmil.com>
2023-10-30 14:47:04 +0000

    changes made

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+//Importing the input and output library
+use std::io;
+
+fn main(){
+    println!("It's time for some guess game");
+    println!("Enter your guess game number");
+
+//Declaring and assigning the variables
+    let mut guess = String::new();
+
+    //These are emoji unicodes
+    let happy_face = '\u{1F600}';
+    let sad_face = '\u{1F610}';
+
+
+ 
+ //This is where the program takes and reads the input from the user   
+        io::stdin()
+        .read_line(&mut guess)
+
+        
+        //This code block handles any errors at runtime
+        .expect("Error");
+
+
+        //Here goes the logic for the guess game
+    println!("Your guess number is {guess}");
+    if guess.trim() == "1"{
+        println!("Congratulations{happy_face}...You are right");
+    }
+    else{
+        println!("Sorry try again{sad_face}...You lost");
+    }
 }
